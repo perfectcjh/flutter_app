7570b444a7ec640bfed070654ea2a14e83a31a44
perfectcjh <perfectcjh@foxmail.com>
2019-03-14 17:30:58 +0800

    update

--- lib/pages/goods_page.dart
+++ lib/pages/goods_page.dart
@@ -8,7 +8,18 @@ class GoodsPage extends StatefulWidget {
   }
 }
 
-class GoodsPageState extends State<GoodsPage> {
+class GoodsPageState extends State<GoodsPage> with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
+  List<String> tabs = ["首页", "视频", "小视频", "我的"];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +27,27 @@ class GoodsPageState extends State<GoodsPage> {
         title: Text('商品'),
         backgroundColor: ColorManager.navColor,
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.share), onPressed: () {}),
+          IconButton(icon: Icon(Icons.share), onPressed: () {
+            Navigator.pushNamed(context, 'cgs/test');
+          }),
+          IconButton(icon: Icon(Icons.share), onPressed: () {
+            Navigator.pushNamed(context, 'cgs/web');
+          }),
         ],
+        bottom: TabBar(
+          tabs: tabs.map((e) {
+            return Tab(text: e);
+          }).toList(),
+          controller: _tabController,
+        ),
       ),
+      body: TabBarView(
+          controller: _tabController,
+          children: tabs.map((e) {
+            return Container(
+              alignment: Alignment.center,
+              child: Text(e, textScaleFactor: 5),
+            );
+          }).toList()),
     );
-  }
 }
